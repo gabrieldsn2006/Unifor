df61e24d8489d4095d714fc3c55cf81d500f4e51
Gabsn <gabrieldsn2006@gmail.com>
2024-08-21 15:40:08 -0300

    determinante

--- 2oSemestre/AlgebraGeomComp/AlgebraGeomComp.py
+++ 2oSemestre/AlgebraGeomComp/AlgebraGeomComp.py
@@ -81,3 +81,49 @@ def Sistema(*sis):
     for iii in range(nLinhas): # separando a solução que é apenas a ultima coluna da matriz
         solucao.append(float(f'{sis[iii][nColunas-1]:.1f}'))
     return solucao
+
+ex = [[1, 2, 3, 4, 5],
+      [16, 17, 18, 19, 6],
+      [15, 24, 25, 20, 7],
+      [14, 23, 22, 21, 8],
+      [13, 12, 11, 10, 9]]
+
+ex = [
+[1, 2, 3, 4, 5, 6],
+[20, 21, 22, 23, 24, 7],
+[19, 32, 33, 34, 25, 8],
+[18, 31, 36, 35, 26, 9],
+[17, 30, 29, 28, 27, 10],
+[16, 15, 14, 13, 12, 11],
+]
+
+# matrix calculator site
+
+def det(A):
+    nLinhas = len(A) # nº de linhas da matriz
+    nColunas = len(A[0]) # nº de colunas da matriz
+    if nLinhas != nColunas: return None
+
+    if nLinhas == 2:
+        return (A[0][0] * A[1][1]) - (A[0][1] * A[1][0])
+    else:
+        r = 0
+        for x in range(nColunas):
+            r += A[0][x] * laplace(A, 0, x)
+        return r
+
+def laplace(A, i, j):
+    mat = []
+
+    nLinhas = len(A) # nº de linhas da matriz
+    nColunas = len(A[0]) # nº de colunas da matriz
+
+    for x in range(1, nLinhas):
+        mat.append([])
+        for y in range(nColunas):
+            if y != j:
+                mat[x-1].append(A[x][y])
+
+    return ((-1)**(i+j)) * det(mat)
+
+print(det(ex))
