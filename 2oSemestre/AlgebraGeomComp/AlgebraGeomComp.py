25842d0d0f7c98c6d0ef0a89a4f176e73495df21
Gabsn <gabrieldsn2006@gmail.com>
2024-08-22 14:40:01 -0300

    commit

--- 2oSemestre/AlgebraGeomComp/AlgebraGeomComp.py
+++ 2oSemestre/AlgebraGeomComp/AlgebraGeomComp.py
@@ -3,62 +3,58 @@ class Matriz:
         self.nLinhas = len(vetor)
         self.nColunas = len(vetor[0])
         self.vetor = vetor[:]
-        self.len = len(vetor)
+        self.quadrado = True if self.nLinhas == self.nColunas else False
 
-        self.quadrado = False
-        if self.nLinhas == self.nColunas:
-            self.quadrado = True
-
-        self.linhas = vetor[:]
-        C = list()
-        tr = 0
-        for i in range(self.nColunas):
-            C.append(list())
-            for j in range(self.nLinhas):
-                C[i].append(vetor[j][i])
-                if i == j: tr += vetor[i][j]
-        self.colunas = C
-        self.tr = tr
-        self.transposta = C
-        
-    def dados(self):
-        if self.quadrado:
-            print(f'Sua matriz possui {self.nLinhas} linhas e {self.nColunas} colunas e é quadrada.')
-            print(f'O traço de sua matriz vale {self.tr}')
-        else:
-            print(f'Sua matriz possui {self.nLinhas} linhas e {self.nColunas} colunas.')
-
-    def exibir(self):
-        print(f'Matriz : ')
-        for i in range(self.nLinhas):
-            print(self.linhas[i]) 
-
-def Soma(A, B):
+def exibir(A):
     A = Matriz(A)
-    B = Matriz(B)
-    R = list()
+    print(f'Matriz : ')
     for i in range(A.nLinhas):
-        R.append(list())
-        for j in range(A.nColunas):
-            R[i].append((A.vetor[i][j]) + (B.vetor[i][j]))
-    return R
+        print(A.vetor[i])
 
-def Produto(A, B):
+def transposta(A):
+    A = Matriz(A)
+    t = list()
+    for i in range(A.nColunas):
+        t.append(list())
+        for j in range(A.nLinhas):
+            t[i].append(A.vetor[j][i])
+    return t
+
+def tr(A): 
+    A = Matriz(A)
+    if A.quadrado:
+        for i in range(A.nLinhas):
+            tr += A.vetor[i][i]
+        return tr
+    else:
+        return None
+
+def soma(A, B):
     A = Matriz(A)
     B = Matriz(B)
-    R = list()
+    S = list()
+    for i in range(A.nLinhas):
+        S.append(list())
+        for j in range(A.nColunas):
+            S[i].append((A.vetor[i][j]) + (B.vetor[i][j]))
+    return S
+
+def produto(A, B):
+    A = Matriz(A)
+    B = Matriz(B)
+    P = list()
     
     for i in range(A.nLinhas):
-        r = list()
+        aux = list()
         for k in range(B.nColunas):
             sum = 0
             for j in range(A.nColunas):
                 sum += A.vetor[i][j] * B.vetor[j][k]
-            r.append(sum)
-        R.append(r)
-    return R
+            aux.append(sum)
+        P.append(aux)
+    return P
 
-def Sistema(*sis):
+def sistema(*sis):
     nLinhas = len(sis) # nº de linhas da matriz
     nColunas = len(sis[0]) # nº de colunas da matriz
 
@@ -81,36 +77,6 @@ def Sistema(*sis):
     for iii in range(nLinhas): # separando a solução que é apenas a ultima coluna da matriz
         solucao.append(float(f'{sis[iii][nColunas-1]:.1f}'))
     return solucao
-
-ex = [
-[1, 2],
-[4, 3]
-]
-
-ex0 = [
-[1, 2, 3],
-[8, 9, 4],
-[7, 6, 5]
-]
-
-ex1 = [
-[1, 2, 3, 4, 5],
-[16, 17, 18, 19, 6],
-[15, 24, 25, 20, 7],
-[14, 23, 22, 21, 8],
-[13, 12, 11, 10, 9]
-]
-
-ex2 = [
-[1, 2, 3, 4, 5, 6],
-[20, 21, 22, 23, 24, 7],
-[19, 32, 33, 34, 25, 8],
-[18, 31, 36, 35, 26, 9],
-[17, 30, 29, 28, 27, 10],
-[16, 15, 14, 13, 12, 11],
-]
-
-# https://matrixcalc.org
 
 def det(A):
     nLinhas = len(A) # nº de linhas da matriz
@@ -138,5 +104,3 @@ def laplace(A, i, j):
                 mat[x-1].append(A[x][y])
 
     return ((-1)**(i+j)) * det(mat)
-
-print(det(ex))
