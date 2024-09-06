8d918cb083b2572139093f99b9e83b59480e3024
Gabsn <gabrieldsn2006@gmail.com>
2024-09-06 12:42:12 -0300

    Update Algebra.py

--- 2oSemestre/AlgebraGeomComp/Cenario_AV1/Algebra.py
+++ 2oSemestre/AlgebraGeomComp/Cenario_AV1/Algebra.py
@@ -183,4 +183,4 @@ class LinearAlgebra:
 
 # também fiz um método para calcular determinante e inversa
 # o codigo completo pode ser acessado nesse link:
-# https://github.com/gabrieldsn2006/Unifor/blob/main/2oSemestre/AlgebraGeomComp/AlgebraGeomComp.py
+# https://github.com/gabrieldsn2006/Unifor/blob/main/2oSemestre/AlgebraGeomComp/AlgebraGeomComp01.py
