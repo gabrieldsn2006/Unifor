f2910e1efb14f331b9428f4d5ece29330e8765e7
Gabsn <gabrieldsn2006@gmail.com>
2024-09-20 13:19:08 -0300

    Commit

--- Extra/Complexidade de Algoritmos/Fibonacci/Fib_O(1).py
+++
@@ -1,8 +0,0 @@
-import math
-
-def fib(n):
-    phi = (1 + math.sqrt(5)) / 2
-    return int((phi ** n - (1 - phi) ** n) / math.sqrt(5))
-
-n = int(input('Termo da sequência que deseja encontrar: '))
-print(f'Encontrado com apenas 1 execução, o {n}º termo da sequência é: {fib(n)}')
--- Extra/Complexidade de Algoritmos/Fibonacci/Fib_O(2^n).py
+++
@@ -1,9 +0,0 @@
-calls = 0
-def fib(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: global calls; calls += 2; return fib(n-1) + fib(n-2)
-
-n = int(input('Termo da sequência que deseja encontrar: '))
-fib_n = fib(n)
-print(f'Encontrado com {calls} chamadas, o {n}º termo da sequência é: {fib_n}')
