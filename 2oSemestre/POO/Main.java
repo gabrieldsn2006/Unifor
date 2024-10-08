d19bd5555f52d59af7c8ccdd6b85c738a9e06c5a
Gabsn <gabrieldsn2006@gmail.com>
2024-10-08 13:44:26 -0300

    Update Main.java

--- 2oSemestre/POO/Main.java
+++ 2oSemestre/POO/Main.java
@@ -1,17 +1,14 @@
-package POOM24EF.Vey;
-
 import java.util.Scanner;
 
 public class Main {
     static Scanner entrada = new Scanner(System.in);
 
-    static char[][] matrix = new char[][] {{' ', ' ', 'A', ' ', ' ', ' ', 'B', ' ', ' ', ' ', 'C', ' '}, {'1', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '}, {' ', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, {'2', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '}, {' ', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, {'3', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '}};
-
-    static char[][] gameMatrix = matrix.clone();
+    static char[][] gameMatrix = new char[][] {{' ', ' ', 'A', ' ', ' ', ' ', 'B', ' ', ' ', ' ', 'C', ' '}, {'1', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '}, {' ', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, {'2', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '}, {' ', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}, {'3', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' '}};
 
     static boolean playing = true;
 
     public static void main(String[] args) {
+
         while (true) {
             printGame();
 
@@ -24,6 +21,12 @@ public class Main {
                     System.out.println("JOGADOR 'X' GANHOU!");
                     break;
                 }
+                turnos++;
+
+                if (turnos == 9) {
+                    System.out.println("EMPATE");
+                    break;
+                }
 
                 play('O');
                 printGame();
@@ -32,18 +35,14 @@ public class Main {
                     System.out.println("JOGADOR 'O' GANHOU!");
                     break;
                 }
-
                 turnos++;
-                if (turnos == 9) {
-                    System.out.println("EMPATE");
-                    break;
-                }
             }
 
             System.out.print("Jogar outra partida? (S/N) ");
             String userInput = ((entrada.next()).toUpperCase()).strip();
             if (userInput.equals("N")) break;
-            gameMatrix = matrix.clone();
+            reset();
+            playing = true;
         }
     }
 
@@ -59,22 +58,24 @@ public class Main {
     public static void play(char xo) {
         while (true) {
             System.out.printf("Sua jogada [%s] ", xo);
-            char[] play = (((entrada.next()).toUpperCase()).strip()).toCharArray();
+            char[] play = (((entrada.nextLine()).toUpperCase()).strip()).toCharArray();
 
-            if ((play[0] == 'A' || play[0] == 'B' || play[0] == 'C') && (play[1] == '1' || play[1] == '2' || play[1] == '3')) {
-                int[] actualPlay = new int[2];
+            if (play.length == 2) {
+                if ((play[0] == 'A' || play[0] == 'B' || play[0] == 'C') && (play[1] == '1' || play[1] == '2' || play[1] == '3')) {
+                    int[] actualPlay = new int[2];
 
-                if (play[0] == 'A') actualPlay[0] = 2;
-                if (play[0] == 'B') actualPlay[0] = 6;
-                if (play[0] == 'C') actualPlay[0] = 10;
+                    if (play[0] == 'A') actualPlay[0] = 2;
+                    if (play[0] == 'B') actualPlay[0] = 6;
+                    if (play[0] == 'C') actualPlay[0] = 10;
 
-                if (play[1] == '1') actualPlay[1] = 1;
-                if (play[1] == '2') actualPlay[1] = 3;
-                if (play[1] == '3') actualPlay[1] = 5;
+                    if (play[1] == '1') actualPlay[1] = 1;
+                    if (play[1] == '2') actualPlay[1] = 3;
+                    if (play[1] == '3') actualPlay[1] = 5;
 
-                if (gameMatrix[actualPlay[1]][actualPlay[0]] == ' ') {
-                    gameMatrix[actualPlay[1]][actualPlay[0]] = xo;
-                    return;
+                    if (gameMatrix[actualPlay[1]][actualPlay[0]] == ' ') {
+                        gameMatrix[actualPlay[1]][actualPlay[0]] = xo;
+                        return;
+                    }
                 }
             }
             System.out.println("INSIRA UMA JOGADA VALIDA!");
@@ -111,6 +112,14 @@ public class Main {
         if (gameMatrix[1][10] == xo && gameMatrix[3][6] == xo && gameMatrix[5][2] == xo) {
             playing = false;
             return;
+        }
+    }
+
+    public static void reset() {
+        for (int i = 1; i <= 5; i = i + 2) {
+            gameMatrix[i][2] = ' ';
+            gameMatrix[i][6] = ' ';
+            gameMatrix[i][10] = ' ';
         }
     }
 }
